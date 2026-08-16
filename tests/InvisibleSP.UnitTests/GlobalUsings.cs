697ac879de8953de61eeaf3790d054073c403545
Weslley Luiz <37993565+weslleymurdock@users.noreply.github.com>
2026-08-16 18:05:35 -0300

    test: add cryptographic helpers for identity coverage

--- tests/InvisibleSP.UnitTests/GlobalUsings.cs
+++ tests/InvisibleSP.UnitTests/GlobalUsings.cs
@@ -2,6 +2,8 @@ global using System;
 global using System.Collections.Generic;
 global using System.Linq;
 global using System.Security.Claims;
+global using System.Security.Cryptography;
+global using System.Text;
 global using System.Threading;
 global using System.Threading.Tasks;
 global using FluentAssertions;
