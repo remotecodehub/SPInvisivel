da0c339a23706eed5547c86ead4b991e7334d73b
Weslley Luiz <37993565+weslleymurdock@users.noreply.github.com>
2026-08-15 21:45:19 -0300

    build: add application global usings

---
+++ src/InvisibleSP.Application/GlobalUsings.cs
@@ -0,0 +1,8 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using FluentValidation;
+global using Mediator.Net;
+global using Mediator.Net.Contracts;
